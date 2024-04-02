a12ed7aaf65b9bac14aad0f629c88d0098c68023
cuixuefeng <cuixuefeng@langjuai.com>
2024-04-02 17:02:25 +0800

    框架搭建中1.0.9

---
+++ code-authentication/src/main/java/org/example/authentication/configure/AuthorizationServerConfig.java
@@ -0,0 +1,44 @@
+﻿//package org.example.authentication.configure;
+//
+//import cn.binarywang.wx.miniapp.api.WxMaService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@Slf4j
+//public class AuthorizationServerConfig {
+//    @Autowired
+//    private WxMaService wxMaService;
+//
+//    private MemberDetailsService memberDetailsService;
+//
+////    @Bean
+////    public SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity httpSecurity){
+////        httpSecurity
+////                // 禁用basic明文验证
+////                // 前后端分离架构不需要csrf保护
+////                // 禁用默认登录页
+////                // 禁用默认登出页
+////                // 设置异常的EntryPoint，如果不设置，默认使用Http403ForbiddenEntryPoint
+////                .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(invalidAuthenticationEntryPoint))
+////                // 前后端分离是无状态的，不需要session了，直接禁用。
+//////                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+////                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+////                        // 允许所有OPTIONS请求
+////                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+////                        // 允许直接访问授权登录接口
+////                        .requestMatchers(HttpMethod.POST, "/web/authenticate").permitAll()
+////                        // 允许 SpringMVC 的默认错误地址匿名访问
+////                        .requestMatchers("/error").permitAll()
+////                        // 其他所有接口必须有Authority信息，Authority在登录成功后的UserDetailsImpl对象中默认设置“ROLE_USER”
+////                        //.requestMatchers("/**").hasAnyAuthority("ROLE_USER")
+////                        // 允许任意请求被已登录用户访问，不检查Authority
+////                        .anyRequest().authenticated())
+////                .authenticationProvider(authenticationProvider())
+////                // 加我们自定义的过滤器，替代UsernamePasswordAuthenticationFilter
+////                .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+////
+////        return httpSecurity.build();
+////    }
+//}
---
+++ code-common/common-security/src/main/java/org/example/common/security/configure/ResourceServerConfig.java
@@ -0,0 +1,123 @@
+package org.example.common.security.configure;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.json.JSONUtil;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.example.common.core.constant.JwtClaimConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.List;
+
+/**
+ * 资源服务器配置
+ *
+ * @author ComeTomorrow
+ * @since 2024/4/2
+ */
+@ConfigurationProperties(prefix = "security")
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class ResourceServerConfig {
+
+    @Autowired
+
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
+    /**
+     * 白名单路径列表
+     */
+    @Setter
+    private List<String> whitelistPaths;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspect) throws Exception {
+
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspect);
+
+        log.info("whitelist path:{}", JSONUtil.toJsonStr(whitelistPaths));
+        http.authorizeHttpRequests((requests) ->
+                    {
+                        //循环白名单路径列表，匹配的路径进行放行
+                        if (CollectionUtil.isNotEmpty(whitelistPaths)) {
+                            for (String whitelistPath : whitelistPaths) {
+                                requests.requestMatchers(mvcMatcherBuilder.pattern(whitelistPath)).permitAll();
+                            }
+                        }
+                        //不在白名单范围的请求需要认证
+                        requests.anyRequest().authenticated();
+                    }
+                ).csrf(AbstractHttpConfigurer::disable);
+        http.oauth2ResourceServer(resourceServerConfigurator ->
+                resourceServerConfigurator
+                        .jwt(jwtConfigurer -> jwtAuthenticationConverter())
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler)
+        );
+        return http.build();
+    }
+
+    /**
+     *
+     * 不走过滤器链的放行配置
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+                AntPathRequestMatcher.antMatcher("/webjars/**"),
+                AntPathRequestMatcher.antMatcher("/doc.html"),
+                AntPathRequestMatcher.antMatcher("/swagger-resources/**"),
+                AntPathRequestMatcher.antMatcher("/v3/api-docs/**"),
+                AntPathRequestMatcher.antMatcher("/swagger-ui/**")
+        );
+    }
+
+    @Bean
+    public MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspect) {
+        return new MvcRequestMatcher.Builder(introspect);
+    }
+
+    /**
+     * 自定义JWT Converter
+     *
+     * @return Converter
+     * @see JwtAuthenticationProvider#setJwtAuthenticationConverter(Converter)
+     */
+    @Bean
+    public Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(Strings.EMPTY);
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName(JwtClaimConstants.AUTHORITIES);
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+
+}
